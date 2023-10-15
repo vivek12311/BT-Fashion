0d083f5df458828d5fd48d65a1249d701c5376a8
Vivek <vivekmalle@Vs-MacBook.local>
2023-10-14 23:35:24 -0500

    check

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vivek</Text>
+      <Text>hello</Text>
       <StatusBar style="auto" />
+      <Button title="tap me" />
     </View>
   );
 }
